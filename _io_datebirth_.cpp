3ae9907987660e3b201e57841965cf264960161a
jose.sbrandao <jose.sbrandao@gmail.com>
2019-08-04 23:17:53 -0300

    date birth I/O testing

---
+++ _io_datebirth_.cpp
@@ -0,0 +1,9 @@
+include <iostream>
+using namespace std;
+int main()
+{
+int dia, mes, ano;
+cout << "Entre com a data do seu aniversario (dd mm aa): ";
+cin >> dia >> mes >> ano;
+cout << "Voce nasceu em " << dia << "/" << mes << "/" << ano << endl;
+}
